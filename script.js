d0599cde7562428451ca84b9b03c7e67133c9e60
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-31 14:38:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn=document.getElementById("incrementButton");
+const counter=document.querySelector("#counter")
+btn.onclick=(()=>{
+	alert(counter.textContent);
+	counter.textContent++;
+	})
