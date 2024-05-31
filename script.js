526d35b2c60fa22905af0a770db99331ce9ac292
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-05-31 15:05:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const btn=document.getElementById("incrementBtn");
-const counter=document.querySelector("#counter")
+const counter=document.getElementById("counter")
 btn.onclick=(()=>{
 	alert(counter.textContent);
 	counter.textContent = parseInt(counter.textContent) + 1
